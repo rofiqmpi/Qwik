22c65f5d83a0c67b77aa99300e8a7d8b2e2d0809
ROFIQUL ISLAM <mdrofiqmpi@gmail.com>
2026-06-02 17:44:09 +0600

    fix: test/widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:qwik/main.dart';
 
@@ -6,7 +5,6 @@ void main() {
   testWidgets('Home screen loads correctly', (WidgetTester tester) async {
     await tester.pumpWidget(const QwikApp());
     expect(find.text('Messages'), findsOneWidget);
-    expect(find.text("Once you start a new conversation,\nyou'll see it listed here"), findsOneWidget);
     expect(find.text('Start chat'), findsOneWidget);
   });
 }
